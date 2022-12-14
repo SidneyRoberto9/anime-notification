b5b37386da0f9daf3a93e2a84b8599c2ddce43db
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-12-14 14:59:18 -0300

    ✨ feat: use http module

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { SendNotification } from '../../app/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
+import { NotificationsController } from './controllers/notification.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [SendNotification],
+})
+export class HttpModule {}
