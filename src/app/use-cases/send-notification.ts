45dc0fd43275eac4569450f8a4bb4a623704a531
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-12-14 14:58:32 -0300

    ✨ feat: Create SendNotification use Case

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+
+import { Notification } from '../entities/Notification/notification';
+import { NotificationRepository } from '../repositories/notification.repository';
+
+interface SendNotificationRequest {
+  title: string;
+  platform: string;
+  platformUrl: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+@Injectable()
+export class SendNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { title, platform, platformUrl } = request;
+
+    const notification = new Notification({
+      title,
+      platform,
+      platformUrl,
+    });
+
+    await this.notificationRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
