096b5bc16335152efe2d0c6a4381a60616d415d5
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-12-14 14:58:48 -0300

    ✨ feat: Create notification Controller

---
+++ src/infra/http/controllers/notification.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { SendNotification } from '../../../app/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { title, platform, platformUrl } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      title,
+      platform,
+      platformUrl,
+    });
+    return { notification };
+  }
+}
